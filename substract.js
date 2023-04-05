7f70a51251b72fae65eed11f593f5e6e2f565a42
Ricardo Ferraz <ricardo.paneo@gmail.com>
2023-04-05 22:00:22 +0200

    Revert "handle error for substract method"
    This reverts commit 512895a25d83f081aadbf42e2a6f74de47886283.

--- substract.js
+++ substract.js
@@ -1,8 +1,7 @@
-module.exports = function substrac(a, b) {
-  const res = a - b
-  if(Number.isNaN(res)) {
-    throw new Error('Invalid Operation')
+module.exports = function substract(a, b) {
+  if (a > b) {
+    throw new error('Negative result not allowed')
   }
- 
-  return res
-}
+  
+    return a - b
+  }
