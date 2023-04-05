6c5621479c423c36175a8c82a7d2f5efe878ab40
Ricardo Ferraz <ricardo.paneo@gmail.com>
2023-04-05 22:19:36 +0200

    handle error for multiply  method

--- multiply.js
+++ multiply.js
@@ -1,3 +1,7 @@
-module.exports = function sum(a, b) {
-    return a - b
+module.exports = function multiply(a, b) {
+  const res = a * b
+  if(Number.isNaN(res)) {
+      throw new error('invalisd Operation')
   }
+  return res
+}
